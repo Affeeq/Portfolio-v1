c0e285ab2e461b2bbd07bed91df76bdf45890886
Affeeq <affeeqabedeenismail@gmail.com>
2020-01-17 17:44:17 +0000

    Npm init, install express ejs bodyParser, set up get routes for landing profile, listen route, set up views dir with partials landing profile ejs

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-console.log("Hello World")
-console.log("Push into GitHub")
+var express 	= require("express"),
+	app 		= express(),
+	bodyParser 	= require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/",function(req,res) {
+	res.render("landing");
+});
+
+app.get("/profile", function(req,res) {
+	res.render("profile");
+})
+
+app.listen(process.env.PORT || 3000, function() {
+	console.log("Connected to server");
+});
