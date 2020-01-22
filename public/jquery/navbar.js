f04c979deea352cd3c0435453e8bb4fcb62dda1b
Affeeq <affeeqabedeenismail@gmail.com>
2020-01-22 18:41:40 -0500

    Add slide up/down animation on navbar when scroll up/down

--- public/jquery/navbar.js
+++ public/jquery/navbar.js
@@ -1,3 +1,19 @@
+$(function () {
+	var lastScroll = 0;
+	$(document).scroll(function(){
+		var navbarHeight = $(".navbar").innerHeight();
+		var currentScroll = $(window).scrollTop();
+		if(currentScroll > navbarHeight && currentScroll > lastScroll) {
+			$(".navbar").addClass("navbarHideDisplay").removeClass("navbarTransition");
+			lastScroll = currentScroll;
+		} else if(currentScroll > navbarHeight && currentScroll < lastScroll) {
+			$(".navbar").addClass("navbarShowDisplay").removeClass("navbarHideDisplay");
+			lastScroll = currentScroll;
+		}
+	});
+})
+
+
 $(".nav-link").click(function() {
 	$(this).addClass("active");
 	$(".nav-link").not(this).removeClass("active");
