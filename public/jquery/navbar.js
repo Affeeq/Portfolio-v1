4390df3ec0abd520e6bccdd84d5edf729cca589d
Affeeq <affeeqabedeenismail@gmail.com>
2020-02-05 16:22:12 -0500

    Major Change in Struture, into a one pager

--- public/jquery/navbar.js
+++ public/jquery/navbar.js
@@ -2,19 +2,32 @@
 $(function () {
 	var lastScroll = 0;
 	$(document).scroll(function(){
+		$("#navbarTarget").removeClass("show");
 		var navbarHeight = $(".navbar").innerHeight();
 		var currentScroll = $(window).scrollTop();
 		if(currentScroll > navbarHeight && currentScroll > lastScroll) {
 			$(".navbar").addClass("navbarHideDisplay").removeClass("navbarShowDisplay").removeClass("navbarTransparent");
+			$(".navbar-brand").addClass("navbar-scrolled");
 			lastScroll = currentScroll;
 		} else {
-			$(".navbar").addClass("navbarShowDisplay").addClass("navbarShowDisplayColor").removeClass("navbarHideDisplay");
+			$(".navbar").addClass("navbarShowDisplay").removeClass("navbarHideDisplay");
 			lastScroll = currentScroll;
 			if(currentScroll < navbarHeight) {
-				$(".navbar").addClass("navbarTransparent").removeClass("navbarShowDisplay").removeClass("navbarShowDisplayColor");
-				console.log(currentScroll)
 				console.log(navbarHeight)
+				console.log(currentScroll)
+				$(".navbar").addClass("navbarTransparent").removeClass("navbarShowDisplay");
+				$(".navbar-brand").removeClass("navbar-scrolled");
 			}
 		}
+		
+		$("section").each(function() {
+			var id = $(this).attr("id");
+			if(currentScroll < $("header").innerHeight()) {
+				$(".nav-item").removeClass("navbar-active");
+			} else if(currentScroll >= $(this).offset().top){
+				$(".nav-item").removeClass("navbar-active");
+				$("#"+id+"-link").addClass("navbar-active");
+			}
+		})
 	});
 })
