84edb56652385c6000d71861fabbf158729a0d72
Affeeq <affeeqabedeenismail@gmail.com>
2020-02-15 14:59:00 -0500

    First upload version

--- public/jquery/navbar.js
+++ public/jquery/navbar.js
@@ -2,32 +2,38 @@
 $(function () {
 	var lastScroll = 0;
 	$(document).scroll(function(){
-		$("#navbarTarget").removeClass("show");
 		var navbarHeight = $(".navbar").innerHeight();
 		var currentScroll = $(window).scrollTop();
 		if(currentScroll > navbarHeight && currentScroll > lastScroll) {
-			$(".navbar").addClass("navbarHideDisplay").removeClass("navbarShowDisplay").removeClass("navbarTransparent");
-			$(".navbar-brand").addClass("navbar-scrolled");
+			$(".navbar").addClass("navbarShowDisplay").removeClass("navbarTransparent");
 			lastScroll = currentScroll;
 		} else {
-			$(".navbar").addClass("navbarShowDisplay").removeClass("navbarHideDisplay");
 			lastScroll = currentScroll;
 			if(currentScroll < navbarHeight) {
-				console.log(navbarHeight)
-				console.log(currentScroll)
 				$(".navbar").addClass("navbarTransparent").removeClass("navbarShowDisplay");
-				$(".navbar-brand").removeClass("navbar-scrolled");
 			}
 		}
 		
 		$("section").each(function() {
 			var id = $(this).attr("id");
-			if(currentScroll < $("header").innerHeight()) {
+			if(currentScroll < $("header").innerHeight() - ($("header").innerHeight() * 0.25)) {
 				$(".nav-item").removeClass("navbar-active");
-			} else if(currentScroll >= $(this).offset().top){
+			} else if($(window).scrollTop() + $(window).height() == $(document).height()) {
+				$(".nav-item").removeClass("navbar-active");
+				$("#contact" + "-link").addClass("navbar-active");
+			} else if(currentScroll >= $(this).offset().top - 1) {
 				$(".nav-item").removeClass("navbar-active");
 				$("#"+id+"-link").addClass("navbar-active");
 			}
 		})
 	});
 })
+
+$( ".nav-link" ).click(function() {
+  $(".navbar-toggler:visible").click();
+});
+
+
+$('.modal').on('shown.bs.modal', function() {
+	$(".modal").css("padding",'0px 5px');
+});
