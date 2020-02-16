a8dd4b4631476815b25d90f58b1c14855f7bd7c4
Affeeq <affeeqabedeenismail@gmail.com>
2020-02-15 22:36:36 -0500

    fix smooth scroll problem

--- public/jquery/navbar.js
+++ public/jquery/navbar.js
@@ -17,18 +17,57 @@ $(function () {
 		$("section").each(function() {
 			var id = $(this).attr("id");
 			if(currentScroll < $("header").innerHeight() - ($("header").innerHeight() * 0.25)) {
-				$(".nav-item").removeClass("navbar-active");
+				$(".nav-link").removeClass("navbar-active");
 			} else if($(window).scrollTop() + $(window).height() == $(document).height()) {
-				$(".nav-item").removeClass("navbar-active");
+				$(".nav-link").removeClass("navbar-active");
 				$("#contact" + "-link").addClass("navbar-active");
 			} else if(currentScroll >= $(this).offset().top - 1) {
-				$(".nav-item").removeClass("navbar-active");
+				$(".nav-link").removeClass("navbar-active");
 				$("#"+id+"-link").addClass("navbar-active");
 			}
 		})
 	});
 })
 
+// animate scrolling when links are clicked
+$("#intro-link, #landing-link-down").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#intro").offset().top
+    }, 1500);
+});
+
+$("#focus-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#focus").offset().top
+    }, 1500);
+});
+
+$("#experience-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#experience").offset().top
+    }, 1500);
+});
+
+$("#project-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#project").offset().top
+    }, 1500);
+});
+
+$("#contact-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 1500);
+});
+
+$(".footer-icon-up").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#landing-img-container").offset().top
+    }, 1500);
+});
+
+
+// close navbar when nav-link is clicked
 $( ".nav-link" ).click(function() {
   $(".navbar-toggler:visible").click();
 });
