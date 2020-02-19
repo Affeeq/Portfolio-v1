2c68460e0786bcaf602936a6b3edcfd72318b33d
Affeeq <affeeqabedeenismail@gmail.com>
2020-02-19 12:10:51 -0500

    Add post route for contact form

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express 	= require("express"),
 	app 		= express(),
-	bodyParser 	= require("body-parser");
+	bodyParser 	= require("body-parser"),
+	nodemailer 	= require("nodemailer"),
+	async 		= require("async");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
@@ -10,16 +12,8 @@ app.get("/",function(req,res) {
 	res.render("landing");
 });
 
-app.get("/about", function(req,res) {
-	res.render("about");
-});
+app.post("/contact", function(req,res,next) {
 
-app.get("/project", function(req,res) {
-	res.render("project");
-});
-
-app.get("/contact", function(req,res) {
-	res.render("contact");
 });
 
 app.listen(process.env.PORT || 3000, function() {
